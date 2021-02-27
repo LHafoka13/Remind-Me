e8f2cb992ac19f862db6c4f7809645134c32d71b
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-02-27 13:16:23 -0700

    updated file structure

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Kahuku.13",
+  database: "remind_me",
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected!");
+});
+
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
