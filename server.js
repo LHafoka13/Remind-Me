50443f5f64288d4fa01a863585477eabb24962a3
Shawn McPhee <scmcphee9@gmail.com>
2021-03-01 22:22:21 -0700

    updated server and added .env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./client/public/models");
+// const mysql = require("mysql2");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,21 +13,21 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// const mysql = require("mysql");
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PW,
-//   database: "remind_me",
-// });
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: "remind_me",
+});
 
-// con.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("Connected!");
-// });
+con.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected!");
+});
 
 app.use(function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
