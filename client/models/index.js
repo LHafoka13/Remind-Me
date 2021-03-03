68eb604e024e3102abdae75b9dfad0e5abd05477
Shawn McPhee <scmcphee9@gmail.com>
2021-03-02 19:14:02 -0700

    updated config and models location

--- client/models/index.js
+++ client/models/index.js
@@ -1,15 +1,16 @@
+"use strict";
+
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-
-const basename = path.basename(module.filename);
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
-let sequelize;
 
+let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -26,7 +27,10 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
