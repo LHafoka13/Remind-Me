1b347c2229f828f51a8bfa4c717dd23b378d759b
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-03-02 20:36:22 -0700

    updated the reigster page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Remind Me
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -96,16 +83,13 @@ export default function Home() {
             </Button>
             <Grid container>
               <Grid item>
-                <Link href="#" >
+                <Link to="/register" >
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
             </Grid>
           </form>
         </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
       </Container>
     </div>
   );
