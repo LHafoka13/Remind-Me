abca2dce89a8a392348a6967ca7fa7f67c35aa29
Lindsay Bordelon <lcbordelon@gmail.com>
2021-03-11 21:15:38 -0700

    full connection from backend users table to client side

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const appointmentRoutes = require("./appointments");
+const userRoutes = require("./users");
 
 router.use("/appointment", appointmentRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
