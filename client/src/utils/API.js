c0a68fa9a905b64f2574322778a49d1a74d2c4bc
Lindsay Bordelon <lcbordelon@gmail.com>
2021-03-12 14:06:28 -0700

    started passport code on client and backend :sparkles:

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,21 @@ export default {
   getUsers: function() {
     return axios.get("/api/users");
   },
+
+  // logs in user
+  login: function(loginInfo) {
+    return axios.post("/api/users/login", loginInfo);
+  },
+
+  signup: function(signupInfo) {
+    return axios.post("/api/users/signup", signupInfo);
+  },
+
+  isLoggedIn: function() {
+    return axios.get("/api/users/profile");
+  },
+
+  logout: function() {
+    return axios.get("/api/users/logout");
+  },
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,78 @@
 // Requiring our models
 
 const db = require("../../models");
+const passport = require("../../config/passport");
+const authMiddleware = require("../../config/middleware/isAuthenticated");
 
 module.exports = (app) => {
-  // GET route for getting all of the appointments
+  app.post(
+    "/login",
+    passport.authenticate("local", {
+      failureRedirect: "/api/users/unauthorized",
+      failureFlash: true,
+    }),
+    function(req, res, next) {
+      console.log("sign in successful");
+      res.json({
+        user: req.user,
+        loggedIn: true,
+      });
+    }
+  );
+
+  //post route for adding new user to the db
+  app.post("/signup", function(req, res, next) {
+    db.User.findOne({ email: req.body.email }, function(err, user) {
+      if (err) throw err;
+      if (user) {
+        console.log("user already exists");
+        return res.json("user already exists");
+      }
+      if (!user) {
+        db.User.findOne({ email: req.body.email }, function(err, useremail) {
+          if (err) throw err;
+          if (useremail) {
+            return res.json("email is already in use");
+          }
+          if (!useremail) {
+            let newUser = new db.User({
+              firstname: req.body.firstname,
+              lastname: req.body.lastname,
+              email: req.body.email,
+              password: req.body.password,
+              helper: req.body.helper,
+            });
+            newUser.password = newUser.generateHash(req.body.password);
+            newUser.save(function(err) {
+              if (err) throw err;
+              console.log("user saved!");
+              res.redirect(307, "/api/users/login");
+            });
+          }
+        });
+      }
+    });
+  });
+
+  app.get("/unauthorized", function(req, res, next) {
+    res.json({
+      error: req.flash("error"),
+      message: "user not authenticated",
+    });
+  });
+
+  app.get("/profile", authMiddleware.isLoggedIn, function(req, res, next) {
+    res.json({
+      email: req.email,
+      loggedIn: true,
+    });
+  });
+
+  app.get("/logout", authMiddleware.logoutUser, function(req, res, next) {
+    res.json("User logged out successfully");
+  });
+
+  // GET route for getting all of the users
   // findAll returns all entries for a table when used with no options
   app.get("/api/users", (req, res) => {
     db.User.findAll({}).then((allUsers) => {
