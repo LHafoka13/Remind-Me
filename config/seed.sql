2cd76a5a4cc8996f90d0b69e5d7c92e5663ee2fa
Lindsay Bordelon <lcbordelon@gmail.com>
2021-03-13 13:11:54 -0700

    pushing successfully from frontend users to db

--- config/seed.sql
+++ config/seed.sql
@@ -1,2 +1,2 @@
 INSERT INTO appointments (title, startDate, endDate, description, member, rRule, createdAt, updatedAt) VALUES ("Take Pills", new Date(2021, 03, 09, 00:00:00), new Date(2021, 03, 09, 01:00:00), "Take all 5 pills", "", 'FREQ=DAILY;COUNT=3', CURDATE(), CURDATE());
-INSERT INTO users (firstName, lastName, email, password, helper, createdAt, updatedAt) VALUES ("tubby", "bordelon", "tubby@gmail.com", "meow", true, CURDATE(), CURDATE());
+INSERT INTO users (firstName, lastName, email, password, helper, member, createdAt, updatedAt) VALUES ("tubby", "bordelon", "tubby@gmail.com", "meow", true, false, CURDATE(), CURDATE());
