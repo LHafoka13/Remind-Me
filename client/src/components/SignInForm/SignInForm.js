e29f5565bdad7a465570ca3b7dd160395b1523ba
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-03-13 09:56:07 -0700

    updated the userObjectModel within the registration page to toggle values

--- client/src/components/SignInForm/SignInForm.js
+++ client/src/components/SignInForm/SignInForm.js
@@ -12,6 +12,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
