71ae8ccc932113ec69b34ab22aacec5b51f8bf7a
Robby Ray <rgr5035@gmail.com>
2021-03-14 20:37:47 -0600

    updated models to reflect new values

--- config/seed.sql
+++ config/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO appointments (title, startDate, description, member, rRule, createdAt, updatedAt, UserId) VALUES ("Take Pills", "2021-03-12 16:00:00", "Take all 5 pills", "", 'FREQ=DAILY;COUNT=3', CURDATE(), CURDATE(), 2);
+INSERT INTO appointments (title, startDate, description, rRule, createdAt, updatedAt, UserId) VALUES ("Take Pills", "2021-03-14 16:00:00", "Take all 5 pills", 'FREQ=DAILY;COUNT=3', CURDATE(), CURDATE(), 2);
 
 
 
